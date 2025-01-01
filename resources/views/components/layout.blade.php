645ce2a4286435ae2b61d3dc1ec40f2298f846df
pdemianowicz <pdemianowicz@gmail.com>
2025-01-01 13:20:57 +0100

    Initial commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Driving Theory Test</title>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+
+  @vite('resources/css/app.css')
+</head>
+<body>
+
+  <header class="shadow">
+    <nav class="flex items-center gap-5 max-w-screen-lg px-12 py-4 mx-auto">
+      <a href="/" class="mr-auto text-2xl font-bold text-blue-500">Driving Theory Test</a>
+      <a href="">Link 1</a>
+      <a href="">Link 2</a>
+      <a href="">Link 3</a>
+    </nav>
+  </header>
+
+  <main class="container">
+    {{ $slot }}
+  </main>
+
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+  <h1 class="text-center p-4">Driving Theory Test - Testy Teoretyczne Na Prawo Jazdy!!</h1>
+</x-layout>
